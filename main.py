24a0458f5796637060f4f178337cb2291e03b580
Artyom Danilov <defytheflow@gmail.com>
2020-05-23 00:09:19 +0300

    Remove left middle frame and add 3 more empty buttons

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import tkinter as tk
 
 from PIL import Image, ImageTk
 
-
 WIN_WIDTH = 1200
 WIN_HEIGHT = 900
 
@@ -12,16 +11,19 @@ BTN_HEIGHT = 110
 
 BG_COLOR = '#c9b662'
 
-# Create a main window.
-window = tk.Tk()
+# --------------------------------------------------------------------------------
+# MAIN WINDOW.
+# --------------------------------------------------------------------------------
 
-# Configure main window.
+window = tk.Tk()
 window.title('Medieval')
 window.geometry(f'{WIN_WIDTH}x{WIN_HEIGHT}')
 window.resizable(0, 0)
 
-# Create top frame.
 # --------------------------------------------------------------------------------
+# TOP FRAME.
+# --------------------------------------------------------------------------------
+
 top_frame = tk.Frame(window, height=WIN_HEIGHT // 9)
 top_frame.pack(fill=tk.BOTH)
 
@@ -31,39 +33,39 @@ hint_label = tk.Label(
 )
 hint_label.pack(fill=tk.BOTH)
 
-# Create middle frame.
 # --------------------------------------------------------------------------------
+# MIDDLE FRAME.
+# --------------------------------------------------------------------------------
+
 middle_frame = tk.Frame(window)
 middle_frame.pack(fill=tk.BOTH)
 
-# middle left frame
-middle_left_frame = tk.Frame(middle_frame)
-middle_left_frame.pack(fill=tk.BOTH, side=tk.LEFT)
+# MAIN CANVAS.
+main_canvas = tk.Canvas(
+    middle_frame, width=WIN_WIDTH * 0.9, height=WIN_HEIGHT * 7 / 9,  bg='#000', relief=tk.FLAT
+)
+main_canvas.pack(fill=tk.BOTH, side=tk.LEFT)
 
-# settings button
+main_canvas_image = ImageTk.PhotoImage(
+    Image.open(os.path.join('assets','bazaar.jpg')).resize((int(WIN_WIDTH * 0.8), int(WIN_HEIGHT * 7 / 9)))
+)
+main_canvas_image_id = main_canvas.create_image(0, 0, image=main_canvas_image, anchor=tk.NW)
+
+# MIDDLE RIGHT FRAME.
+middle_right_frame = tk.Frame(middle_frame)
+middle_right_frame.pack(fill=tk.BOTH, side=tk.LEFT)
+
+# SETTINGS BUTTON.
 settings_btn_img = ImageTk.PhotoImage(
     Image.open(os.path.join('assets', 'settings-btn.png')).resize((BTN_WIDTH, BTN_HEIGHT))
 )
 settings_btn = tk.Button(
-    middle_left_frame, image=settings_btn_img, bg=BG_COLOR, bd=5,
+    middle_right_frame, image=settings_btn_img, bg=BG_COLOR, bd=5,
     highlightbackground='#000', activebackground='#7f6f28',
 )
 settings_btn.pack(fill=tk.BOTH)
 
-# main canvas.
-main_canvas = tk.Canvas(
-    middle_frame, width=WIN_WIDTH * 0.8, height=WIN_HEIGHT * 7 / 9,  bg='#000', relief=tk.FLAT
-)
-main_canvas.pack(fill=tk.BOTH, side=tk.LEFT)
-
-main_canvas_image = ImageTk.PhotoImage(Image.open(os.path.join('assets', 'bazaar.jpg')))
-main_canvas_image_id = main_canvas.create_image(0, 0, image=main_canvas_image, anchor=tk.NW)
-
-# middle right frame.
-middle_right_frame = tk.Frame(middle_frame)
-middle_right_frame.pack(fill=tk.BOTH, side=tk.LEFT)
-
-# map button.
+# MAP BUTTON.
 map_btn_img = ImageTk.PhotoImage(
     Image.open(os.path.join('assets', 'map-btn.png')).resize((BTN_WIDTH, BTN_HEIGHT))
 )
@@ -73,7 +75,7 @@ map_btn = tk.Button(
 )
 map_btn.pack(fill=tk.BOTH)
 
-# inventory button.
+# INVENTORY BUTTON.
 inventory_btn_img = ImageTk.PhotoImage(
     Image.open(os.path.join('assets', 'inventory-btn.png')).resize((BTN_WIDTH, BTN_HEIGHT))
 )
@@ -83,12 +85,33 @@ inventory_btn = tk.Button(
 )
 inventory_btn.pack(fill=tk.BOTH)
 
-# Create bottom frame.
+# OTHER BUTTONS.
+btn4 = tk.Button(
+    middle_right_frame, bg=BG_COLOR, bd=5, text='4',
+    highlightbackground='#000', activebackground='#7f6f28',
+)
+btn4.pack(fill=tk.BOTH, expand=True)
+
+btn5 = tk.Button(
+    middle_right_frame, bg=BG_COLOR, bd=5, text='5',
+    highlightbackground='#000', activebackground='#7f6f28',
+)
+btn5.pack(fill=tk.BOTH, expand=True)
+
+btn6 = tk.Button(
+    middle_right_frame, bg=BG_COLOR, bd=5, text='6',
+    highlightbackground='#000', activebackground='#7f6f28',
+)
+btn6.pack(fill=tk.BOTH, expand=True)
+
 # --------------------------------------------------------------------------------
+# BOTTOM FRAME.
+# --------------------------------------------------------------------------------
+
 bottom_frame = tk.Frame(window)
 bottom_frame.pack(fill=tk.BOTH, expand=True)
 
-# bottom canvas.
+# BOTTOM CANVAS.
 bottom_canvas = tk.Canvas(
     bottom_frame, width=180, bg=BG_COLOR, relief=tk.RAISED, highlightbackground='#000', bd=5
 )
@@ -99,18 +122,18 @@ bottom_canvas_image = ImageTk.PhotoImage(
 )
 bottom_canvas_image_id = bottom_canvas.create_image(0, 0, image=bottom_canvas_image, anchor=tk.NW)
 
-# bottom right frame.
+# BOTTOM RIGHT FRAME.
 bottom_right_frame = tk.Frame(bottom_frame, width=WIN_WIDTH - 180, bg=BG_COLOR, bd=5, relief=tk.RAISED)
 bottom_right_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-# dialogue label.
+# DIALOGUE LABEL.
 dialogue_label = tk.Label(
     bottom_right_frame, text='Do you want to start?', fg='#181c21', bg=BG_COLOR,
     anchor=tk.NW, relief=tk.RAISED, bd=5, padx=10
 )
 dialogue_label.pack(fill=tk.BOTH)
 
-# choice buttons.
+# CHOICE BUTTONS.
 choice_var = tk.IntVar()
 
 choice_btn_img = ImageTk.PhotoImage(
