b8fa90d4c27beabd467e21be23b7da4fca5908f6
Artyom Danilov <defytheflow@gmail.com>
2020-05-22 22:42:51 +0300

    Style bottom frame and hint label

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 
+WIN_WIDTH = 1200
+WIN_HEIGHT = 900
+
 BTN_WIDTH = 110
 BTN_HEIGHT = 110
 
@@ -13,27 +16,28 @@ window = tk.Tk()
 
 # Configure main window.
 window.title('Medieval')
-window.geometry('1200x900')
+window.geometry(f'{WIN_WIDTH}x{WIN_HEIGHT}')
 window.attributes('-alpha', 1.0)
+window.resizable(0, 0)
 
 # Create top frame.
 # --------------------------------------------------------------------------------
-top_frame = tk.Frame(window, height=150)
+top_frame = tk.Frame(window, height=WIN_HEIGHT // 9)
 top_frame.pack(fill=tk.BOTH)
 
 hint_label = tk.Label(
-    top_frame, text='Hint: Pirates are bad!', font=('Helvetica', '24'), bg='#181c21', fg='#c9b662',
-    anchor=tk.NW, relief=tk.FLAT, pady=10, padx=10, bd=2,
+    top_frame, text='Hint: Pirates are bad!', fg='#181c21', bg='#c9b662',
+    anchor=tk.NW, relief=tk.RAISED, bd=5, padx=10
 )
 hint_label.pack(fill=tk.BOTH)
 
 # Create middle frame.
 # --------------------------------------------------------------------------------
-middle_frame = tk.Frame(window, height=600)
+middle_frame = tk.Frame(window)
 middle_frame.pack(fill=tk.BOTH)
 
 # middle left frame
-middle_left_frame = tk.Frame(middle_frame, height=600, width=120)
+middle_left_frame = tk.Frame(middle_frame)
 middle_left_frame.pack(fill=tk.BOTH, side=tk.LEFT)
 
 # settings button
@@ -47,14 +51,16 @@ settings_btn = tk.Button(
 settings_btn.pack(fill=tk.BOTH)
 
 # main canvas.
-main_canvas = tk.Canvas(middle_frame, width=960, height=600, bg='#000', relief=tk.FLAT)
+main_canvas = tk.Canvas(
+    middle_frame, width=WIN_WIDTH * 0.8, height=WIN_HEIGHT * 7 / 9,  bg='#000', relief=tk.FLAT
+)
 main_canvas.pack(fill=tk.BOTH, side=tk.LEFT)
 
 main_canvas_image = ImageTk.PhotoImage(Image.open(os.path.join('assets', 'bazaar.jpg')))
 main_canvas_image_id = main_canvas.create_image(0, 0, image=main_canvas_image, anchor=tk.NW)
 
 # middle right frame.
-middle_right_frame = tk.Frame(middle_frame, height=600, width=120)
+middle_right_frame = tk.Frame(middle_frame)
 middle_right_frame.pack(fill=tk.BOTH, side=tk.LEFT)
 
 # map button.
@@ -77,28 +83,26 @@ inventory_btn = tk.Button(
 )
 inventory_btn.pack(fill=tk.BOTH)
 
-
 # Create bottom frame.
 # --------------------------------------------------------------------------------
-bottom_frame = tk.Frame(window, height=150)
+bottom_frame = tk.Frame(window)
 bottom_frame.pack(fill=tk.BOTH, expand=True)
-
-# bottom left frame
-bottom_left_frame = tk.Frame(
-    bottom_frame, width=950, height=150, bg='#c9b662'
-)
-bottom_left_frame.pack(fill=tk.BOTH, side=tk.LEFT)
 
 # bottom canvas.
 bottom_canvas = tk.Canvas(
-    bottom_frame, width=250, height=150, bg='#c9b662', relief=tk.FLAT, highlightbackground='#000'
+    bottom_frame, width=180, bg='#c9b662', relief=tk.RAISED, highlightbackground='#000', bd=5
 )
 bottom_canvas.pack(fill=tk.BOTH, side=tk.LEFT)
 
 bottom_canvas_image = ImageTk.PhotoImage(
-    Image.open(os.path.join('assets', 'wizard.png')).resize((250, 250))
+    Image.open(os.path.join('assets', 'wizard.png')).resize((200, 200))
 )
 bottom_canvas_image_id = bottom_canvas.create_image(0, 0, image=bottom_canvas_image, anchor=tk.NW)
+
+# bottom right frame
+bottom_right_frame = tk.Frame(
+    bottom_frame, width=WIN_WIDTH - 180, bg='#c9b662', bd=5, relief=tk.RAISED)
+bottom_right_frame.pack(fill=tk.BOTH, side=tk.LEFT)
 
 if __name__ == '__main__':
     window.mainloop()
