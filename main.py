1d36ecfd72d3b40685ece352825510982743c503
Artyom Danilov <defytheflow@gmail.com>
2020-05-24 14:28:12 +0300

    Refactor main.py and widgets.py

--- main.py
+++ main.py
@@ -4,69 +4,31 @@ import os
 import tkinter as tk
 
 from utils import create_photo_image
-from widgets import ToolTip, SettingsFrame
+from widgets import Button, Label, Radiobutton, ToolTip, SettingsFrame
 
-# -----------------------------------------------------------------------------
-# CONSTANTS.
-# -----------------------------------------------------------------------------
 
 WIN_WIDTH = 1200
 WIN_HEIGHT = 900
 
-BG_COLOR = '#c9b662'             # beige.
-BD_COLOR = '#000'                # black.
+BG_COLOR = '#c9b662'
 
-BTN_WIDTH = 110
-BTN_HEIGHT = 110
-BTN_BD = 5
-BTN_ACTIVE_BG_COLOR = '#7f6f28'  # dark beige.
-
-LBL_PADX = 10
-LBL_FG_COLOR = '#000'
-LBL_BD = 5
-
-# -----------------------------------------------------------------------------
-# WINDOW.
-# -----------------------------------------------------------------------------
 
 window = tk.Tk()
 window.title('Medieval')
 window.geometry(f'{WIN_WIDTH}x{WIN_HEIGHT}')
 window.resizable(0, 0)
 
-# -----------------------------------------------------------------------------
-# TOP FRAME.
-# -----------------------------------------------------------------------------
 
 top_frame = tk.Frame(window)
 top_frame.pack(fill=tk.BOTH)
 
-# -----------------------------------------------------------------------------
-# HINT LABEL.
-# -----------------------------------------------------------------------------
-
-hint_lbl = tk.Label(
-    top_frame,
-    text='Hint: Pirates are bad!',
-    bg=BG_COLOR,
-    fg=LBL_FG_COLOR,
-    bd=LBL_BD,
-    padx=LBL_PADX,
-    relief=tk.RAISED,
-    anchor=tk.NW,
-)
+hint_lbl = Label(top_frame, text='Hint: Pirates are bad!')
 hint_lbl.pack(fill=tk.BOTH)
 
-# -----------------------------------------------------------------------------
-# MIDDLE FRAME.
-# -----------------------------------------------------------------------------
 
 middle_frame = tk.Frame(window)
 middle_frame.pack()
 
-# -----------------------------------------------------------------------------
-# MAIN CANVAS.
-# -----------------------------------------------------------------------------
 
 main_canvas = tk.Canvas(
     middle_frame,
@@ -85,16 +47,10 @@ main_canvas_image_id = main_canvas.create_image(
     0, 0, image=main_canvas_image, anchor=tk.NW
 )
 
-# -----------------------------------------------------------------------------
-# MIDDLE RIGHT FRAME.
-# -----------------------------------------------------------------------------
 
 middle_right_frame = tk.Frame(middle_frame)
 middle_right_frame.pack(side=tk.LEFT, fill=tk.BOTH)
 
-# -----------------------------------------------------------------------------
-# BUTTONS.
-# -----------------------------------------------------------------------------
 
 def create_settings_frame(master: tk.Tk):
 
@@ -105,76 +61,49 @@ def create_settings_frame(master: tk.Tk):
     frame.pack(fill=tk.BOTH, expand=True)
 
 
-btn_attrs = {
-    'bg': BG_COLOR,
-    'bd': BTN_BD,
-    'highlightbackground': BD_COLOR,
-    'activebackground': BTN_ACTIVE_BG_COLOR,
-}
-
-settings_btn_image = create_photo_image(
-    os.path.join('assets', 'settings-btn.png'),
-    (BTN_WIDTH, BTN_HEIGHT),
+settings_btn = Button(
+    middle_right_frame,
+    file=os.path.join('assets', 'settings-icon.png'),
+    tooltip='Settings',
 )
-
-map_btn_image = create_photo_image(
-    os.path.join('assets', 'map-btn.png'),
-    (BTN_WIDTH, BTN_HEIGHT),
-)
-inventory_btn_image = create_photo_image(
-    os.path.join('assets', 'inventory-btn.png'),
-    (BTN_WIDTH, BTN_HEIGHT),
-)
-
-settings_btn = tk.Button(middle_right_frame, image=settings_btn_image, **btn_attrs)
 settings_btn.pack(fill=tk.BOTH)
+settings_btn.bind('<Button-1>', lambda e: create_settings_frame(window))
 
-settings_tooltip = ToolTip('Settings')
-settings_btn.bind('<Enter>', settings_tooltip.show)
-settings_btn.bind('<Leave>', settings_tooltip.hide)
-settings_btn.bind('<Button-1>', lambda _: create_settings_frame(window))
-
-map_btn = tk.Button(middle_right_frame, image=map_btn_image, **btn_attrs)
+map_btn = Button(
+    middle_right_frame,
+    file=os.path.join('assets', 'map-icon.png'),
+    tooltip='Map'
+)
 map_btn.pack(fill=tk.BOTH)
 
-map_tooltip = ToolTip('Map')
-map_btn.bind('<Enter>', map_tooltip.show)
-map_btn.bind('<Leave>', map_tooltip.hide)
-
-inventory_btn = tk.Button(middle_right_frame, image=inventory_btn_image, **btn_attrs)
+inventory_btn = Button(
+    middle_right_frame,
+    file=os.path.join('assets', 'inventory-icon.png'),
+    tooltip='Inventory'
+)
 inventory_btn.pack(fill=tk.BOTH)
 
-inventory_tooltip = ToolTip('Inventory')
-inventory_btn.bind('<Enter>', inventory_tooltip.show)
-inventory_btn.bind('<Leave>', inventory_tooltip.hide)
-
-btn4 = tk.Button(middle_right_frame, text='4', **btn_attrs)
+btn4 = Button(middle_right_frame)
 btn4.pack(fill=tk.BOTH, expand=True)
 
-btn5 = tk.Button(middle_right_frame, text='5', **btn_attrs)
+btn5 = Button(middle_right_frame)
 btn5.pack(fill=tk.BOTH, expand=True)
 
-btn6 = tk.Button(middle_right_frame, text='6', **btn_attrs)
+btn6 = Button(middle_right_frame)
 btn6.pack(fill=tk.BOTH, expand=True)
 
-# -----------------------------------------------------------------------------
-# BOTTOM FRAME.
-# -----------------------------------------------------------------------------
 
 bottom_frame = tk.Frame(window)
 bottom_frame.pack(fill=tk.BOTH)
 
-# -----------------------------------------------------------------------------
-# BOTTOM CANVAS.
-# -----------------------------------------------------------------------------
 
 bottom_canvas = tk.Canvas(
     bottom_frame,
     width=180,
-    bg=BG_COLOR,
-    bd=5,
+    background=BG_COLOR,
+    borderwidth=5,
+    highlightbackground='#000',
     relief=tk.RAISED,
-    highlightbackground=BD_COLOR,
 )
 bottom_canvas.pack(side=tk.LEFT, fill=tk.BOTH)
 
@@ -186,69 +115,26 @@ bottom_canvas_image_id = bottom_canvas.create_image(
     0, 0, image=bottom_canvas_image, anchor=tk.NW
 )
 
-# -----------------------------------------------------------------------------
-# BOTTOM RIGHT FRAME.
-# -----------------------------------------------------------------------------
 
 bottom_right_frame = tk.Frame(
     bottom_frame,
     width=WIN_WIDTH - bottom_canvas.winfo_reqwidth(),
-    bg=BG_COLOR,
-    bd=5,
+    background=BG_COLOR,
+    borderwidth=5,
     relief=tk.RAISED,
 )
 bottom_right_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-# -----------------------------------------------------------------------------
-# DIALOGUE LABEL.
-# -----------------------------------------------------------------------------
-
-dialogue_lbl = tk.Label(
-    bottom_right_frame,
-    text='Do you want to start?',
-    bg=BG_COLOR,
-    fg=LBL_FG_COLOR,
-    bd=LBL_BD,
-    padx=LBL_PADX,
-    relief=tk.RAISED,
-    anchor=tk.NW,
-)
+dialogue_lbl = Label(bottom_right_frame, text='Do you want to start?')
 dialogue_lbl.pack(fill=tk.BOTH)
 
-# -----------------------------------------------------------------------------
-# CHOICE BUTTONS.
-# -----------------------------------------------------------------------------
 
 choice_var = tk.IntVar()
 
-choice_btn_image = create_photo_image(
-    os.path.join('assets', 'choice-btn.png'),
-    (180, 90),
-)
-
-choice_btn_attrs = {
-    'variable': choice_var,
-    'image': choice_btn_image,
-    'bg': BG_COLOR,
-    'activebackground': BG_COLOR,
-    'highlightbackground': BG_COLOR,
-    'compound': tk.CENTER,
-}
-
-choice_btn1 = tk.Radiobutton(
-    bottom_right_frame,
-    text='Yes',
-    value=1,
-    **choice_btn_attrs,
-)
+choice_btn1 = Radiobutton(bottom_right_frame, text='Yes', value=1, variable=choice_var)
 choice_btn1.pack(side=tk.LEFT, expand=True)
 
-choice_btn2 = tk.Radiobutton(
-    bottom_right_frame,
-    text='No',
-    value=2,
-    **choice_btn_attrs,
-)
+choice_btn2 = Radiobutton(bottom_right_frame, text='No', value=2, variable=choice_var)
 choice_btn2.pack(side=tk.LEFT, expand=True)
 
 if __name__ == '__main__':
--- widgets.py
+++ widgets.py
@@ -1,27 +1,90 @@
+"""
+    Custom widgets.
+"""
+
 import os
 import tkinter as tk
 
 from utils import create_photo_image
 
 
+class Button(tk.Button):
+    """
+        Styled tk.Button.
+    """
+
+    def __init__(self, parent, file='', tooltip='', **kwargs):
+        self.image = create_photo_image(file, (110, 110)) if file else None
+        super().__init__(parent,
+                         image=self.image,
+                         background='#c9b662',
+                         borderwidth=5,
+                         highlightbackground='#000',
+                         activebackground='#7f6f28',
+                         relief=tk.RAISED,
+                         **kwargs)
+        if tooltip:
+            self.tooltip = ToolTip(tooltip)
+            self.bind('<Enter>', self.tooltip.show)
+            self.bind('<Leave>', self.tooltip.hide)
+
+
+class Label(tk.Label):
+    """
+        Styled tk.Label.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,
+                         **kwargs,
+                         background='#c9b662',
+                         foreground='#000',
+                         borderwidth=5,
+                         padx=10,
+                         relief=tk.RAISED,
+                         anchor=tk.NW)
+
+
+class Radiobutton(tk.Radiobutton):
+    """
+        Styled tk.Radiobutton.
+    """
+
+    def __init__(self, *args, **kwargs):
+        file = os.path.join('assets', 'choice-icon.png')
+        self.image = create_photo_image(file, (180, 90))
+        super().__init__(*args,
+                         **kwargs,
+                         image=self.image,
+                         background='#c9b662',
+                         activebackground='#c9b662',
+                         highlightbackground='#c9b662',
+                         compound=tk.CENTER)
+
+
 class ToolTip:
+    """
+        Info box shown over a widget.
+    """
 
     def __init__(self, text):
-        self.toplevel = None
         self.text = text
-        self.lbl_padx = 10
+        self.padx = 10
+        self.toplevel = None
 
     def show(self, event):
-        x = event.widget.winfo_rootx() - len(self.text) - self.lbl_padx
+        x = event.widget.winfo_rootx() - len(self.text) - self.padx
         y = event.widget.winfo_rooty()
+
         self.toplevel = tk.Toplevel(event.widget)
-        self.toplevel.wm_geometry('+%d+%d' % (x, y))
+        self.toplevel.wm_geometry(f'+{x}+{y}')
         self.toplevel.wm_overrideredirect(1)
+
         label = tk.Label(self.toplevel,
                          text=self.text,
                          background='#c9b662',
-                         bd=3,
-                         padx=self.lbl_padx,
+                         borderwidth=3,
+                         padx=self.padx,
                          font=('DejaVu Serif', '12', 'italic'),
                          relief=tk.RAISED)
         label.pack(fill=tk.BOTH, expand=True)
@@ -33,28 +96,11 @@ class ToolTip:
 
 class SettingsFrame(tk.Frame):
 
-    BACK_BTN_IMAGE = None
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         top_frame = tk.Frame(self, background='#c9b662')
         top_frame.pack(side=tk.TOP, fill=tk.BOTH)
 
-        self.BACK_BTN_IMAGE = create_photo_image(
-            os.path.join('assets', 'back-btn.png'),
-            (80, 60),
-        )
-
-        back_btn = tk.Button(top_frame,
-                             width=100,
-                             height=60,
-                             image=self.BACK_BTN_IMAGE,
-                             bd=5,
-                             background='#c9b662',
-                             highlightbackground='#000',
-                             activebackground='#7f6f28',
-                             relief=tk.RAISED,
-                             )
+        back_btn = Button(top_frame, file=os.path.join('assets', 'back-icon.png'))
         back_btn.pack(side=tk.LEFT)
-
