134cebbe35fd2bf9b86032bf86ffe0f3abc2e63e
Artyom Danilov <defytheflow@gmail.com>
2020-05-26 00:32:28 +0300

    Add map level preview

--- map_frame.py
+++ map_frame.py
@@ -7,17 +7,14 @@ from widgets import TitleFrame
 
 class MapFrame(TitleFrame):
 
+    PREVIEW_WIDTH = 300
+    PREVIEW_HEIGHT = 150
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args,
                          text_dict={'eng': 'Map', 'rus': 'Карта'},
                          **kwargs)
 
-        self.canvas = None
-        self.canvas_image = None
-
-        self._init_map_frame_components()
-
-    def _init_map_frame_components(self):
         self.canvas = tk.Canvas(
             self,
             width=self['width'],
@@ -26,8 +23,14 @@ class MapFrame(TitleFrame):
             highlightbackground=self['background'],
         )
         self.canvas.pack(fill=tk.BOTH, expand=True)
+
         self.canvas_image = utils.create_photo_image(
             os.path.join('assets', 'map.png'),
-            (int(self.canvas['width']), int(self.canvas['height']))
-        )
+            (int(self.canvas['width']), int(self.canvas['height'])))
+
+        self.bazaar_image = utils.create_photo_image(
+            os.path.join('assets', 'bazaar-preview.png'),
+            (self.PREVIEW_WIDTH, self.PREVIEW_HEIGHT))
+
         self.canvas.create_image(0, 0, image=self.canvas_image, anchor=tk.NW)
+        self.canvas.create_image(110, 170, image=self.bazaar_image, anchor=tk.NW)
