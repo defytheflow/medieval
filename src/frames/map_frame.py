d6ea9758aa7689fd5938c65eeb403c9244ee78d4
Artyom Danilov <defytheflow@gmail.com>
2020-06-02 23:55:51 +0300

    Clean up game_frame.py, add new button click sound

--- src/frames/map_frame.py
+++ src/frames/map_frame.py
@@ -1,10 +1,17 @@
 import tkinter as tk
 
 import config
-
 from canvases import MapCanvas
-from widgets import TitleFrame
-from widgets.behavior import KeyboardBoundWidget, MouseBoundWidget
+
+from widgets import (
+    TitleFrame,
+)
+
+from widgets.behavior import (
+    KeyboardBoundWidget,
+    MouseBoundWidget,
+)
+
 from widgets.utils import get_widget_parent
 
 
@@ -13,18 +20,20 @@ class MapFrame(tk.Frame, MouseBoundWidget):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        self.title_frame = TitleFrame(self, bg=self['bg'], font=config.H_FONT,
-            text_dict={'eng': 'Map', 'rus': 'Карта'})
+        self.title_frame = TitleFrame(self,
+                                      background=self['bg'],
+                                      font=config.H_FONT,
+                                      text_dict={'eng': 'Map', 'rus': 'Карта'})
 
-        self.canvas = MapCanvas(self, bg=self['bg'],
-            width=self.winfo_reqwidth(),
-            height=self.winfo_reqheight() - self.title_frame.return_btn.winfo_reqheight(),
-            highlightbackground=self['bg'])
+        self.canvas = MapCanvas(self,
+                                width=self.winfo_reqwidth(),
+                                height=self.winfo_reqheight() - self.title_frame.winfo_reqheight(),
+                                background=self['bg'],
+                                highlightbackground=self['bg'])
 
         self.title_frame.pack(fill='both')
         self.canvas.pack(fill='both', expand=True)
 
     # Overrides MouseBoundWidget.
     def init_mouse_binds(self) -> None:
-        self.title_frame.return_btn.bind('<1>',
-            lambda e: get_widget_parent(self).show_frame('game'))
+        self.title_frame.return_btn.bind('<1>', lambda e: get_widget_parent(self).show_frame('game'))
